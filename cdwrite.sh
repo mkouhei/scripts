740fa36ec1469c4ce5064d289f783a802f31efb9
Kouhei Maeda <mkouhei@palmtb.net>
2010-06-19 14:42:55 +0900

    Revert "Add -overburn option."
    This reverts commit 88602a26feac9f683a9318fbaa31b72213912004.

--- cdwrite.sh
+++ cdwrite.sh
@@ -3,14 +3,13 @@
 test -z $1 && exit 1
 ISOIMAGE=$1
 
-wodim -overburn -v dev='/dev/scd0' speed=10 $ISOIMAGE
+wodim -v dev='/dev/scd0' speed=10 $ISOIMAGE
 
 if [ "$?" -eq 0 ];
 then
 	mount /dev/dvd
 	ls /media/cdrom
 	umount /media/cdrom0
-	sleep 3
 fi
 
 eject cdrom
