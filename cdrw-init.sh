22201afd7d68f918de5ff5d6d948740cafa3df2f
Kouhei Maeda <mkouhei@palmtb.net>
2010-06-19 14:18:16 +0900

    Update for MacBook 5,2.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- cdrw-init.sh
+++ cdrw-init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # MacBook Air Superdrive
-cdrecord -dev=4,0,0 -blank=fast
+#cdrecord -dev=4,0,0 -blank=fast
 
 # MacBook 
-#cdrecord -dev=1,0,0 -blank=fast
+cdrecord -dev=1,0,0 -blank=fast
