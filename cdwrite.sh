88602a26feac9f683a9318fbaa31b72213912004
Kouhei Maeda <mkouhei@palmtb.net>
2010-06-19 14:16:52 +0900

    Add -overburn option.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- cdwrite.sh
+++ cdwrite.sh
@@ -3,13 +3,14 @@
 test -z $1 && exit 1
 ISOIMAGE=$1
 
-wodim -v dev='/dev/scd0' speed=10 $ISOIMAGE
+wodim -overburn -v dev='/dev/scd0' speed=10 $ISOIMAGE
 
 if [ "$?" -eq 0 ];
 then
 	mount /dev/dvd
 	ls /media/cdrom
 	umount /media/cdrom0
+	sleep 3
 fi
 
 eject cdrom
