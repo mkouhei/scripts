0e44fdf8e5ac82a87750cc4236b14e6b09a4bc9b
Kouhei Maeda <mkouhei@palmtb.net>
2010-06-19 14:38:59 +0900

    Update for MacBook 5,2.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- kbdset.sh
+++ kbdset.sh
@@ -9,9 +9,14 @@ setxkbmap
 #xmodmap -e "keycode 212 = Delete"
 
 # for MacBook 5,2 white
-xmodmap -e "keycode 116 = Pointer_Button3"
-xmodmap -e "keycode 113 = Pointer_Button2"
-xmodmap -e "keycode 173 = Delete"
+#xmodmap -e "keycode 116 = Pointer_Button3"
+#xmodmap -e "keycode 113 = Pointer_Button2"
+#xmodmap -e "keycode 173 = Delete"
+
+# for MacBook 5,2 white 2010.05.03.
+xmodmap -e "keycode 134 = Pointer_Button3"
+xmodmap -e "keycode 108 = Pointer_Button2"
+xmodmap -e "keycode 212 = Delete"
 
 xkbset m
 xkbset exp m
