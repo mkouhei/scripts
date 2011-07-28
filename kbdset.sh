7650605676dd822140f09c6765540b7b3ae0043d
Kouhei Maeda <mkouhei@palmtb.net>
2011-07-28 22:19:53 +0900

    Add MacBookPro 8,2 15".
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- kbdset.sh
+++ kbdset.sh
@@ -14,10 +14,13 @@ setxkbmap
 #xmodmap -e "keycode 173 = Delete"
 
 # for MacBook 5,2 white 2010.05.03.
-xmodmap -e "keycode 134 = Pointer_Button3"
-xmodmap -e "keycode 108 = Pointer_Button2"
-xmodmap -e "keycode 212 = Delete"
+#xmodmap -e "keycode 134 = Pointer_Button3"
+#xmodmap -e "keycode 108 = Pointer_Button2"
+#xmodmap -e "keycode 212 = Delete"
 
+# for MacBook 8,2 15"
+xmodmap -e "keycode 62 = Pointer_Button3"
+xmodmap -e "keycode 108 = Pointer_Button2"
 xkbset m
 xkbset exp m
 
