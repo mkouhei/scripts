7bc43e72da12c816e8c3580fa2fffc71de9416fc
Kouhei Maeda <mkouhei@palmtb.net>
2011-08-05 01:29:18 +0900

    update version.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- skype.sh
+++ skype.sh
@@ -1,16 +1,14 @@
 #!/bin/bash 
 
-SKYPE_DIR=~/download/skype_static-2.1.0.81
+SKYPE_DIR=~/download/skype_static-2.2.0.35
 CMD=skype
 
 case "$1" in
 start)
-sudo chmod a-r /usr/lib32/libpulse{-simple.so.0.0.2,.so.0.8.0,common-0.9.15.so}
 ${SKYPE_DIR}/${CMD} &
 ;;
 
 stop)
-sudo chmod a+r /usr/lib32/libpulse{-simple.so.0.0.2,.so.0.8.0,common-0.9.15.so}
 pkill $CMD
 ;;
 
