79db2363fa367e986520e14a6931d5eeefa34df0
Kouhei Maeda <mkouhei@palmtb.net>
2017-08-28 07:27:59 +0900

    Updates xmodmap for ThinkPad T470s.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- kbdset.sh
+++ kbdset.sh
@@ -19,8 +19,24 @@ setxkbmap
 #xmodmap -e "keycode 212 = Delete"
 
 # for MacBook 8,2 15"
+#xmodmap -e "keycode 62 = Pointer_Button3"
+#xmodmap -e "keycode 108 = Pointer_Button2"
+
+# for ThinkPad T470s US keyboard
+# right shift
 xmodmap -e "keycode 62 = Pointer_Button3"
-xmodmap -e "keycode 108 = Pointer_Button2"
+
+# right PrintScreen
+xmodmap -e "keycode 107 = Pointer_Button2"
+
+# left alt
+#xmodmap -e "keycode 64 = Meta_L"
+
+# capslock
+xmodmap -e "keycode 66 = Control_L NoSymbol Control_L"
+xmodmap -e "clear lock"
+xmodmap -e "add Control = Control_L"
+
 xkbset m
 xkbset exp m
 
